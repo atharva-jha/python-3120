6caef3cc548ddf7faeaae703664c194704571307
atharva-jha <atharvajha57@gmail.com>
2024-07-16 09:57:20 +0530

    Create lecture_2.py
    coolage codes of lecture 2 on 19/06/2024

---
+++ lecture_2.py
@@ -0,0 +1,15 @@
+#lecture 2 
+
+name="atharva" 
+print(name[1])
+test="1000"
+print(test[0])
+
+a=int(input("subject 1 "))
+b=int(input("subjnect 2"))
+c=int(input("subject 3"))
+d=int(input("subject 4"))
+e=int(input("subject 5"))
+total_marks = a + b + c + d + e
+percentage = (total_marks / 5)
+print("Final percentage:", percentage)
