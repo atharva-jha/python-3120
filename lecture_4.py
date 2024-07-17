fb6d4e65ea329722a032a5f839214ea357e14986
atharva-jha <atharvajha57@gmail.com>
2024-07-17 10:06:31 +0530

    Update lecture_4.py

--- lecture_4.py
+++ lecture_4.py
@@ -26,3 +26,24 @@ elif budget>5000 and budget<100000:
         print("go away")
 else:
     print("i can draw a laptop or for u")
+
+#code3
+choice=input ("L,D,B");
+if(choice=="B"):
+    food=input("SI or NI");
+    if(food=="SI"):
+        c=input("idle or dosa");
+        if(c=="idle"):
+            print("you ordered idle");
+if(choice=="L"):
+    food=input("Punjabi or bihari");
+    if(food=="bihari"):
+        c=input("chole or littee chokha");
+        if(c=="chole"):
+            print("you ordered chole");
+if (choice=="D"):
+    food=input("maharashtrian or gujrati");
+    if(food=="maharashtrian"):
+        c=input("rajma chawal or biryani");
+        if(c=="biryani"):
+            print("you ordered biryani");
