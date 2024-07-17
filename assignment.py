9cec57175b9adef5b429b6a90e20f2e18ea7a7ef
atharva-jha <atharvajha57@gmail.com>
2024-07-17 13:57:35 +0530

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,15 @@
+import yt_dlp
+
+# Enter the url for the download url = input("Enter video url: ")
+
+ydl_opts = {}
+
+with yt_dlp. YoutubeDL(ydl_opts) as ydl: ydl.download([url])
+
+print("Video downloaded successfully!")
+#code 2
+from colorama import Fore print(Fore. RED + "hello world") print(Fore. BLUE + "hello world") print(Fore. GREEN + "hello world")
+
+print(Fore. YELLOW + "CLCODING.COM")
+
+print(Fore. CYAN + "THANK YOU")
