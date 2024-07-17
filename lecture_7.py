0c86d661aa83a56f97def0e78b70bf77f4abfa82
atharva-jha <atharvajha57@gmail.com>
2024-07-17 10:01:42 +0530

    Create lecture_7.py

---
+++ lecture_7.py
@@ -0,0 +1,23 @@
+#lecture_7
+f_name=input("enter first name")
+m_name=input("enter middle name")
+l_name=input("enter last name")
+#name="_".join([f_name,m_name,l_name])
+print(" ".join([f_name,m_name,l_name]))
+e_email=input("enter your email") 
+ if email.endswith('''com,.org,.edu,.in''')
+    print("valid")
+  else:
+    print("invalid")
+    fname="kausar"
+    fname-fname.lower()
+    @=fname[0:4]
+    DOB= input("DDMMYYYY")
+    b=DOB[0:4]
+    password=a+b
+#code2
+L1 = [1,2,3,4]
+def print_star(L1):
+    for i in L1:
+        print("*" * i)
+print_star(L1)
